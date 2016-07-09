cec99aa7e108f0cbc0695ef473a9b8240738b0bf
Mark Ukotic <Mark Ukotic>
2016-07-09 12:06:57 +1000

    adding pester tests

--- uc_fix.tests.ps1
+++ uc_fix.tests.ps1
@@ -5,15 +5,46 @@ $Module = '.\uc_fix'
 Import-Module $Module
    
 # describes the function Get-LRO
-Describe 'Get-LRO' {
+Describe 'uc_fix Module' {
 
-    It "Has a manifest file" {
-        "$here\.\$Module.psd1" | should Exist 
+    Context 'Test Modules' {
+    
+        It 'Has a manifest file' {
+            "$here\.\$Module.psd1" | should Exist 
+        }
+    
+        It 'Has a module file' {
+            "$here\.\$Module.psm1" | should Exist 
+        }
     }
     
-    It "Has a module file" {
-        "$here\.\$Module.psm1" | should Exist 
+    Context 'Test AppVeyor' {
+    
+        It 'Has an AppVeyor yml file' {
+            "$here\.\appveyor.yml" | should Exist 
+        }
+    
+    }
+    
+    $files = ('README.md')
+    foreach ($file in $files) {
+    
+        Context 'Test files exist' {
+        
+            It "$file should exist" {
+            "$here\.\$file" | Should Exist
+            }
+        }
     }
 
+
+
+        It "$Module is valid PowerShell code" {
+            $psFile = Get-Content -Path "$here\$module.psm1" -ErrorAction Stop
+            $errors = $null
+            $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+            $errors.Count | Should Be 0
+        }
+    
 }
 
